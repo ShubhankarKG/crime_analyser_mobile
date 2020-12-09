c77ec28dfef6112f5a7ed0656c6d6ab4cbfa020f
Harsimran Virk <harsimranvirk99@gmail.com>
2020-12-09 18:12:50 +0530

    added GUSER and GPASSWORD in env

--- gdb.js
+++ gdb.js
@@ -1,7 +1,7 @@
 const neo4j = require("neo4j-driver");
 const driver = neo4j.driver(
   "neo4j://localhost:7687",
-  neo4j.auth.basic("neo4j", "carmelconvent"),
+  neo4j.auth.basic(process.env.GUSER, process.env.GPASSWORD),
   { disableLosslessIntegers: true }
 );
 
